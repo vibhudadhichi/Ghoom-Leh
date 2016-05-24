b311d97440554e3a2714b13ddf578c92ad905fad
vibhudadhichi <vibhudadhikhi@gmail.com>
2016-05-24 22:28:48 +0530

    Slider Screens Added

---
+++ FinalProject/app/src/main/java/com/example/vibhu/finalproject/MainActivity1.java
@@ -0,0 +1,16 @@
+package com.example.vibhu.finalproject;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Vibhu on 24-05-2016.
+ */
+public class MainActivity1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main1);
+    }
+}
